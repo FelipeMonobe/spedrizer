0e10f5f94f264e68aeba45082c2df45b7701a5ae
Felipe Monobe <xinube@gmail.com>
2018-05-16 23:00:22 -0300

    v1

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# spedrizer
-Deal with sped outputs, roughly
+Programa para gerar XLSX de arquivos gerados no programa SPED.
+Este programa respeita a ordem do arquivo TXT original, logo
+se houver hierarquia de códigos, os códigos filhos virão na
+sequência do seu respectivo pai, e o último irmão delimitará
+a presença do próximo pai.
+O arquivo XLSX é gerado na raiz do projeto com o padrão de nome
+"extracao_<codigo de data>.xlsx".
+Good SPEDing.
+
+PS: Não esquecer de escapar espaçamentos no argumento do caminho
+do arquivo (como demonstrado no exemplo abaixo).
+
+USO:
+sintaxe geral:
+node index.js <caminho do arquivo txt> <código do pai> [<código do primeiro filho>, <código do segundo filho>, ...]
+
+único código:
+node index.js /home/amonobe/Documentos/Profissional/Pessoas\ Jurídicas/Helm/Arquivos/EFD-C/2017/4T/piscof1017v6.txt C100
+
+múltiplos códigos:
+node index.js /home/amonobe/Documentos/Profissional/Pessoas\ Jurídicas/Helm/Arquivos/EFD-C/2017/4T/piscof1017v6.txt C500 C501 C505
---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const json2xls = require('json2xls')
+const { promisify } = require('util')
+
+const _readFile = promisify(fs.readFile)
+
+async function main() {
+  const [_, __, caminhoTxt, codigoPai, ...codigosFilhos] = process.argv
+  const padraoLinha = /^\|.+\|$/gm
+  const textoCru = await _readFile(caminhoTxt, 'utf-8')
+  const linhas = textoCru.match(padraoLinha)
+  const linhasParseadas = linhas.map((linha) => {
+    const colunas = linha.split(/\|/g)
+    const dados = colunas.slice(1, colunas.length - 1)
+    const linhaJson = dados.reduce((acumulador, atual, indice) => {
+      return { [indice + 1]: atual, ...acumulador }
+    }, {})
+
+    return linhaJson
+  })
+  const linhasSelecionadas = linhasParseadas.filter((linha) => {
+    return [codigoPai, ...codigosFilhos].includes(linha['1'])
+  })
+
+  const xlsx = json2xls(linhasSelecionadas)
+  const nomeArquivo = `extracao_${Date.now()}.xlsx`
+  
+  return fs.writeFileSync(nomeArquivo, xlsx, 'binary')
+}
+
+main()
